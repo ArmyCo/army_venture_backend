b0f54a1c921ed579db4119a1525155a4123cbd57
jang_jiwon <kooriangman@gmail.com>
2024-05-15 01:09:27 +0900

    [ADD] placeDetail  리뷰, 사진 추가(아직 부족함)

--- models/placeDao.js
+++ models/placeDao.js
@@ -11,25 +11,38 @@ const getPlaceById = async (placeId) => {
     return placeDetail;
 };
 
+const getPlaceImagesByPlaceId = async (placeId) => {
+    const placeImages = await appDataSource.query(
+      `
+      SELECT * FROM place_images
+      WHERE place_id = ?
+      `,
+      [placeId]
+    )
+    return placeImages;
+}
+
 const getReviewByPlaceId = async (placeId) => {
     const reviewDetail = await appDataSource.query(
       `
-      SELECT pr.id AS review_id, pr.user_id, pr.title AS review_title, pr.content AS review_content, pr.visited_date, pr.created_at, pr.updated_at
+      SELECT pr.id AS review_id, pr.user_id, pr.title AS review_title, pr.content AS review_content, pr.visited_date, pr.created_at, pr.updated_at,
+      reviewScores.reviewScores
       FROM place_reviews pr
-      LEFT JOIN(
+      LEFT JOIN (
         SELECT
-        JSON_ARRAYAGG(
-          JSON_OBJECT(
-            "rating_criteria_id", rs.rating_criteria_id,
-            "rating_criteria_name", rc.rating_title,
-            "score", rs.score
-          )
-        ) AS reviewScores
-        FROM review_scores rs, rating_criteria rc
-        WHERE  rs.rating_criteria_id = rc.id
-      ) ON pr.id = rs.review_id
-      AND pr.place_id = ?
-      GROUP BY pr.id
+          rs.review_id,
+          JSON_ARRAYAGG(
+            JSON_OBJECT(
+              "rating_criteria_id", rs.rating_criteria_id,
+              "rating_criteria_name", rc.rating_title,
+              "score", rs.score
+            )
+          ) AS reviewScores
+        FROM review_scores rs
+        JOIN rating_criteria rc ON rs.rating_criteria_id = rc.id
+        GROUP BY rs.review_id
+      ) AS reviewScores ON pr.id = reviewScores.review_id
+      WHERE pr.place_id = ?
       `,
       [placeId]
     )
@@ -38,5 +51,6 @@ const getReviewByPlaceId = async (placeId) => {
 
 module.exports = {
     getPlaceById,
+    getPlaceImagesByPlaceId,
     getReviewByPlaceId
 }
--- services/placeService.js
+++ services/placeService.js
@@ -3,7 +3,8 @@ const { placeDao, appDataSource } = require("../models");
 const gettingPlaceDetails = async (placeId) => {
     const placeDetail = await placeDao.getPlaceById(placeId);
     const reviewList = await placeDao.getReviewByPlaceId(placeId);
-    return { place: placeDetail, reviews: reviewList };
+    const placeImages = await placeDao.getPlaceImagesByPlaceId(placeId);
+    return { place: placeDetail, placeImages: placeImages,reviews: reviewList };
 };
 
 module.exports = {
