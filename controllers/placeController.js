44dec02aada2aae573fdcf84243abf0ecb210067
oxqnd <kkinho04@catholic.ac.kr>
2024-05-16 23:32:52 +0900

    [ADD] freature_review

--- controllers/placeController.js
+++ controllers/placeController.js
@@ -17,7 +17,32 @@ const getPlaceHollidays = catchAsync(async (req, res) => {
   return res.status(200).json({ result });
 });
 
+const getPlaceById = async (req, res) => {
+  try {
+      const placeId = req.params.placeId;
+      const place = await placeService.getPlaceById(placeId);
+      res.status(200).json(place);
+  } catch (error) {
+      res.status(500).json({ message: error.message });
+  }
+};
+
+const addReview = async (req, res) => {
+  try {
+      const userId = req.user.id; // loginRequired 미들웨어를 통과한 경우 user 객체가 req에 포함됩니다.
+      const placeId = req.params.placeId;
+      const { title, content, visitedDate } = req.body;
+
+      await placeService.addReview(userId, placeId, title, content, visitedDate);
+      res.status(201).json({ message: '리뷰가 성공적으로 등록되었습니다.' });
+  } catch (error) {
+      res.status(500).json({ message: error.message });
+  }
+};
+
 module.exports = {
   getPlaceDetails,
-  getPlaceHollidays
+  getPlaceHollidays,
+  addReview,
+  getPlaceById
 };
--- routes/placeRouter.js
+++ routes/placeRouter.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const placeController = require('../controllers/placeController');
+const { loginRequired } = require('../utils/auth');
 
 router.get('/:placeId', placeController.getPlaceDetails);
 router.get('/:placeId/holidays', placeController.getPlaceHollidays);
+router.post('/:placeId/reviews', loginRequired, placeController.addReview);
 
 module.exports = { router };
