c08900f4c4d285ca72187dac6584f7808be4911e
jang_jiwon <kooriangman@gmail.com>
2024-05-17 16:42:02 +0900

    tmp

--- routes/courseRouter.js
+++ routes/courseRouter.js
@@ -2,9 +2,9 @@ const express = require('express');
 const router = express.Router();
 const courseController = require('../controllers/courseController');
 
-router.get('/all', courseController.getAllCourses);
-router.get('/:courseId', courseController.getCourseDetails);
-//router.post('/', courseController.createCourse);
+router.get('/all', loginRequired, courseController.getAllCourses);
+router.get('/:courseId', loginRequired, courseController.getCourseDetails);
+router.post('/', loginRequired, courseController.createCourse);
 //router.patch('/:courseId', courseController.updateCourse);
 
 module.exports = { router };
