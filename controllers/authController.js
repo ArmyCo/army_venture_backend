c7e16daed1874fd4a4bbaa5c4f4beef4fb145bdf
jang_jiwon <kooriangman@gmail.com>
2024-05-18 03:20:05 +0900

    tmp

--- controllers/authController.js
+++ controllers/authController.js
@@ -100,10 +100,14 @@ const signupRedirect = async (req, res) => {
 
 const googleLogin = async (req, res) => {
   try {
+<<<<<<< HEAD
     const token = req.query.authorization; // URL 쿼리에서 토큰 추출
     if (!token) {
       return res.status(401).json({ message: 'Authorization token is missing' });
     }
+=======
+    const googleUserData = req.params; // 프론트엔드에서 구글 OAuth로 받아온 사용자 데이터
+>>>>>>> adc03c4 (tmp)
 
     // 토큰 검증 및 디코드
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
