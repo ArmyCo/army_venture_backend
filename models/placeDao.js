f6575ee3594e14391c93b1832d17e958c8003f34
oxqnd <kkinho04@catholic.ac.kr>
2024-05-18 21:46:43 +0900

    [ADD] review add update delete

--- models/placeDao.js
+++ models/placeDao.js
@@ -56,11 +56,69 @@ const getHollidaysByPlaceId = async (placeId) => {
     return holidays;
 }
 
-const addReview = async (userId, placeId, title, content, visitedDate) => {
+const getReviewsByPlaceId = async (placeId) => {
+  const reviews = await appDataSource.query(
+    `SELECT place_reviews.*, users.name as user_name, review_scores.score as score 
+    FROM place_reviews 
+    JOIN users ON place_reviews.user_id = users.id
+    LEFT JOIN review_scores ON place_reviews.id = review_scores.review_id
+    WHERE place_reviews.place_id = ?`,
+    [placeId]
+  );
+  return reviews;
+};
+
+const getReviewById = async (reviewId) => {
+  const review = await appDataSource.query(
+    `SELECT * FROM place_reviews WHERE id = ?`,
+    [reviewId]
+  );
+  return review[0];
+};
+
+const addReview = async (userId, placeId, title, content, visitedDate, scores) => {
+  try {
+    const result = await appDataSource.query(
+      `INSERT INTO place_reviews (user_id, place_id, title, content, visited_date) 
+      VALUES (?, ?, ?, ?, ?)`,
+      [userId, placeId, title, content, visitedDate]
+    );
+
+    console.log("Scores received in addReview:", scores);
+
+    // scores 처리
+    if (scores && scores.length > 0) {
+      await addReviewScores(result.insertId, scores);
+    } else {
+      console.error("No scores data provided or empty scores array.");
+    }
+
+    return result.insertId;
+  } catch (error) {
+    console.error("Error in addReview:", error);
+    throw new Error("Failed to add review");
+  }
+};
+
+const updateReview = async (reviewId, userId, title, content, visitedDate) => {
   await appDataSource.query(
-    `INSERT INTO place_reviews (user_id, place_id, title, content, visited_date) 
-    VALUES (?, ?, ?, ?, ?)`,
-    [userId, placeId, title, content, visitedDate]
+    `UPDATE place_reviews SET title = ?, content = ?, visited_date = ? 
+    WHERE id = ? AND user_id = ?`,
+    [title, content, visitedDate, reviewId, userId]
+  );
+
+  //await updateReviewScores(reviewId, scores);
+};
+
+const deleteReview = async (reviewId, userId) => {
+  await appDataSource.query(
+    `DELETE FROM review_scores WHERE review_id = ?`,
+    [reviewId]
+  );
+
+  await appDataSource.query(
+    `DELETE FROM place_reviews WHERE id = ? AND user_id = ?`,
+    [reviewId, userId]
   );
 };
 
@@ -70,6 +128,37 @@ const getReviewScores = async (reviewId) => {
     [reviewId]
   );
   return scores;
+};
+
+const addReviewScores = async (reviewId, scores) => {
+  if (!scores || scores.length === 0) {
+    console.error("No scores data provided or scores array is empty.");
+    return;
+  }
+
+  const promises = scores.map(score => {
+    return appDataSource.query(
+      `INSERT INTO review_scores (review_id, place_rating_criteria_id, score)
+      VALUES (?, ?, ?)`,
+      [reviewId, score.ratingCriteriaId, score.score]
+    ).catch(err => console.error('Error inserting score:', err));
+  });
+
+  try {
+    await Promise.all(promises);
+  } catch (error) {
+    console.error("Failed to insert review scores:", error);
+    throw new Error("Failed to insert review scores");
+  }
+};
+
+const updateReviewScores = async (reviewId, scores) => {
+  await appDataSource.query(
+    `DELETE FROM review_scores WHERE review_id = ?`,
+    [reviewId]
+  );
+
+  addReviewScores(reviewId, scores);
 };
 
 const calculateReviewRating = async (placeId) => {
@@ -104,7 +193,13 @@ module.exports = {
     getRatingsByPlaceId,
     checkPlaceCloseInHollidays,
     getHollidaysByPlaceId,
+    getReviewsByPlaceId,
     addReview,
+    updateReview,
+    deleteReview,
     getReviewScores,
-    calculateReviewRating
+    calculateReviewRating,
+    getReviewById,
+    addReviewScores,
+    updateReviewScores
 }
--- services/placeService.js
+++ services/placeService.js
@@ -17,7 +17,32 @@ const gettingHolidays = async (placeId) => {
   return { "message": "This place is open in hollidays" };
 };
 
+const addReview = async (userId, placeId, title, content, visitedDate, scores) => {
+  console.log("Scores in placeService addReview:", scores);
+  const reviewId = await placeDao.addReview(userId, placeId, title, content, visitedDate, scores);
+  const avergeRating = await placeDao.calculateReviewRating(placeId);
+  return {reviewId, avergeRating};
+};
+
+const updateReview = async (reviewId, userId, title, content, visitedDate, scores) => {
+  await placeDao.updateReview(reviewId, userId, title, content, visitedDate);
+  await placeDao.updateReviewScores(reviewId, scores);  // 기존 평점 삭제 후 새로운 평점 추가
+  const review = await placeDao.getReviewById(reviewId);
+  const avergeRating = await placeDao.calculateReviewRating(reviewId);
+  return {review, avergeRating};
+};
+
+
+const deleteReview = async (reviewId, userId) => {
+  await placeDao.deleteReview(reviewId, userId);
+  const avergeRating = await placeDao.calculateReviewRating(reviewId);
+  return avergeRating;
+};
+
 module.exports = {
   gettingPlaceDetails,
-  gettingHolidays
+  gettingHolidays,
+  addReview,
+  updateReview,
+  deleteReview
 }
